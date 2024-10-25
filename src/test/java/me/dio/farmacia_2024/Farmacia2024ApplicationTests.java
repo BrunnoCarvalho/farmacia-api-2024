ea9a2c05b1f65037f60ddb8fdcef00e3cb50fbdc
Bruno Carvalho <brunno_carvalho@outlook.com>
2024-10-25 16:36:59 -0300

    first commit

---
+++ src/test/java/me/dio/farmacia_2024/Farmacia2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.farmacia_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farmacia2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
